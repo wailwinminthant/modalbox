2d4352d00db64e8de1631ebc43d6a43ec4024d4c
Wai Lwin Min Thant <wailwinminthant7@gmail.com>
2024-03-12 22:03:16 +0630

    modalbox

---
+++ app.js
@@ -0,0 +1,42 @@
+// GET UI 
+
+var getmodalbtn = document.getElementById("modal-btn"),
+	getmodalbox = document.getElementById("login-modal"),
+	getbtnclose = document.querySelector(".btn-close");
+
+
+	// getmodalbtn.addEventListener("click",function(){
+	// 	getmodalbox.style.display = "block";
+	// });
+
+	// getbtnclose.addEventListener("click",function(){
+	// 	getmodalbox.style.display = "none";
+	// });
+
+
+	getmodalbtn.addEventListener('click',openmodal);
+	getbtnclose.addEventListener('click',closemodal);
+
+
+	function openmodal(){
+		getmodalbox.style.display = "block";
+	}
+
+	function closemodal(){
+		getmodalbox.style.display = "none";
+	}
+
+	window.onclick = function(e){
+		// console.log(e.target);
+
+
+		if(e.target === getmodalbox){
+			closemodal();
+		}
+	}
+
+
+
+
+
+	//10MB
